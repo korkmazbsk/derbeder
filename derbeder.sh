d507cf3f9cd12b58a945bf6e8ef0cf219f704346
Bayram Korkmaz <70453868+korkmazbsk@users.noreply.github.com>
2024-12-08 04:30:18 +0300

    Update derbeder.sh

--- derbeder.sh
+++ derbeder.sh
@@ -2,4 +2,5 @@
 
 wget https://github.com/DeroLuna/dero-miner/releases/download/v1.14/deroluna-v1.14_linux_hiveos_mmpos.tar.gz
 tar -xvf deroluna-v1.14_linux_hiveos_mmpos.tar.gz
+cd deroluna
 screen -dmS derbeder ./deroluna-miner -w dero1qyn9wt93wcvfzjmf7at5k3kysc7r62pk4zct9vhxmcxzd3gsy84vcqg4tpdgx -d 91.107.226.180:10100
